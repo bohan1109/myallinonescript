6ce7bc8a65d3e992f97343c7d6d602f776779c46
bohan1109 <4A790162@stust.edu.tw>
2022-03-20 21:43:45 +0800

    feat: :fire: install script

---
+++ windows/install.bat
@@ -0,0 +1,54 @@
+:: git
+choco install --yes git
+:: python
+choco install --yes python
+:: SQL Workbench/J 124.0.0
+choco install --yes sql-workbench
+:: Postman for Windows 8.2.3
+choco install --yes postman
+:: Chrome
+choco install -yes googlechrome
+::note pad++
+choco install -yes notepadplusplus.install
+::7-zip
+choco install -yes 7zip
+::zoom
+choco install -yes zoom
+::node.js
+choco install -yes nodejs.install
+::vscode
+choco install -yes vscode
+::powershell
+choco install -yes powershell-core
+::php
+choco install -yes php
+::yarn
+choco install -yes yarn
+::cmder
+choco install -yes cmder
+::classic Shell
+choco install -yes classic-shell
+::Line
+choco install -yes line
+::mysql
+choco install -yes mysql
+::xampp
+choco install -yes bitnami-xampp
+::composer
+choco install -yes composer
+::virtualbox
+choco install -yes virtualbox
+::python pip
+choco install -yes pip
+::teamview
+choco install -yes teamviewer8
+::anydesk
+choco install -yes anydesk
+::vagrant
+choco install -yes vagrant
+::pgAdmin
+choco install -yes pgadmin4
+::docker
+choco install -yes docker
+::ngork
+choco install -yes ngrok
